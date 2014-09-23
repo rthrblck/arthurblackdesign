24166aedf140b9390c5f9ce84863e63f62a80f25
Arthur Black <hellomynameisarthurblack@gmail.com>
2014-09-23 15:30:47 -0700

    Add load-grunt-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -154,19 +157,6 @@ module.exports = function(grunt) {
     }
 
   });
-
-grunt.loadNpmTasks('grunt-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-autoprefixer');
-grunt.loadNpmTasks('grunt-dev-update');
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-clean');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-contrib-imagemin');
-grunt.loadNpmTasks('grunt-gh-pages');
 
 grunt.registerTask('default', ['devUpdate','sass', 'autoprefixer', 'concat', 'connect:devServer', 'watch']);
 grunt.registerTask('test', ['clean', 'cssmin', 'uglify', 'copy', 'imagemin', 'connect:testServer:keepalive']);
