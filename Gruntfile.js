bfb7f3b193f25dded8f2bead7a2420b45b921e6a
Arthur Black <hellomynameisarthurblack@gmail.com>
2014-09-22 18:29:04 -0700

    Setup gh-pages as deploy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,6 +133,7 @@ module.exports = function(grunt) {
         options: {
           progressive: true
         },
+
         files: [{
           expand: true,
           cwd: 'img/',
@@ -142,6 +143,14 @@ module.exports = function(grunt) {
         }
         ]
       }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'build'
+      },
+
+      src: ['**/*']
     }
 
   });
@@ -152,13 +161,15 @@ grunt.loadNpmTasks('grunt-autoprefixer');
 grunt.loadNpmTasks('grunt-dev-update');
 grunt.loadNpmTasks('grunt-contrib-connect');
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-gh-pages');
 
-grunt.registerTask('test', ['clean', 'cssmin', 'uglify', 'copy', 'imagemin', 'connect:testServer:keepalive']);
 grunt.registerTask('default', ['devUpdate','sass', 'autoprefixer', 'concat', 'connect:devServer', 'watch']);
+grunt.registerTask('test', ['clean', 'cssmin', 'uglify', 'copy', 'imagemin', 'connect:testServer:keepalive']);
+grunt.registerTask('deploy', ['gh-pages']);
 
 }
